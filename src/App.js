680348937ef6fed5fa45c1dfa7c65699f1ebe039
gokulakrishnaa <gokulakrishnaapsivam@gmail.com>
2021-11-20 09:04:17 +0530

    Book List

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Home } from "./Home";
+import { Books } from "./Books";
+import { Link, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/books">Books</Link>
+      </nav>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/books">
+          <Books />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export function Home() {
+  return (
+    <div className="home">
+      <h2>Welcome to Book Club</h2>
+      <img
+        src="https://media.istockphoto.com/photos/stack-of-colorful-hardback-books-open-book-on-blue-background-picture-id506196802?k=20&m=506196802&s=612x612&w=0&h=I2J5oWAiGXNIicY0Oj7bW6DjRJBUftjnk6HT2EnLgkU="
+        alt=""
+      ></img>
+    </div>
+  );
+}
